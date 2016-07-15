dd8b636b8acd20bb35d1ac07c85f39fee139c2d4
crazyandcoder <crazyandcoder@gmail.com>
2016-07-15 08:51:44 +0800

    新增以下属性内容：
    滚轮是否循环滚动
    新增文字颜色修改
    新增文字大小修改
    新增滚轮内容可见数量

--- app/src/main/java/com/ihidea/as/citypicker/MainActivity.java
+++ app/src/main/java/com/ihidea/as/citypicker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ihidea.as.citypicker;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,17 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button go=(Button)findViewById(R.id.go);
-        final TextView tvResult=(TextView)findViewById(R.id.tv_result);
+        Button go = (Button) findViewById(R.id.go);
+        final TextView tvResult = (TextView) findViewById(R.id.tv_result);
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CityPickerView cityPickerView=new CityPickerView(MainActivity.this);
+                CityPickerView cityPickerView = new CityPickerView(MainActivity.this);
+                cityPickerView.setTextColor(Color.BLUE);
+                cityPickerView.setTextSize(20);
+                cityPickerView.setVisibleItems(5);
+                cityPickerView.setIsCyclic(true);
                 cityPickerView.show();
                 cityPickerView.setOnCityItemClickListener(new CityPickerView.OnCityItemClickListener() {
                     @Override
                     public void onSelected(String... citySelected) {
-                        tvResult.setText("选择结果：\n省："+citySelected[0]+"\n市："+citySelected[1]+"\n区："+citySelected[2]+"\n邮编："+citySelected[3]);
+                        tvResult.setText("选择结果：\n省：" + citySelected[0] + "\n市：" + citySelected[1] + "\n区：" + citySelected[2] + "\n邮编：" + citySelected[3]);
                     }
                 });
             }
