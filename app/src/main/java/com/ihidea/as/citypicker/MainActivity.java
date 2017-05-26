1a6db6cc76fe96937ea49f4672180996f691a798
crazyandcoder <liji188266>
2017-05-26 09:47:41 +0800

    1、新增城市列表选择器

--- app/src/main/java/com/ihidea/as/citypicker/MainActivity.java
+++ app/src/main/java/com/ihidea/as/citypicker/MainActivity.java
@@ -1,28 +1,46 @@
 package com.ihidea.as.citypicker;
 
+import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.lljjcoder.citylist.CityListSelectActivity;
+import com.lljjcoder.citylist.bean.CityInfoBean;
 import com.lljjcoder.citypickerview.widget.CityPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    TextView tv_resultWheel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button go = (Button) findViewById(R.id.go);
-        final TextView tvResult = (TextView) findViewById(R.id.tv_result);
-        go.setOnClickListener(new View.OnClickListener() {
+        Button goList = (Button) findViewById(R.id.goList);
+        Button goWheel = (Button) findViewById(R.id.goWheel);
+        tv_resultWheel = (TextView) findViewById(R.id.tv_resultWheel);
+        tvResult = (TextView) findViewById(R.id.tv_resultList);
+
+        //城市列表选择器
+        goList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CityListSelectActivity.class);
+                startActivityForResult(intent, CityListSelectActivity.CITY_SELECT_RESULT_FRAG);
+            }
+        });
 
+        //城市滚轮选择器
+        goWheel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 CityPicker cityPicker = new CityPicker.Builder(MainActivity.this).textSize(20)
                         .titleTextColor("#000000")
                         .backgroundPop(0xa0000000)
@@ -41,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 cityPicker.setOnCityItemClickListener(new CityPicker.OnCityItemClickListener() {
                     @Override
                     public void onSelected(String... citySelected) {
-                        tvResult.setText("选择结果：\n省：" + citySelected[0] + "\n市：" + citySelected[1] + "\n区："
+                        tv_resultWheel.setText("选择结果：\n省：" + citySelected[0] + "\n市：" + citySelected[1] + "\n区："
                                 + citySelected[2] + "\n邮编：" + citySelected[3]);
                     }
 
@@ -52,5 +70,37 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CityListSelectActivity.CITY_SELECT_RESULT_FRAG) {
+            if (resultCode == RESULT_OK) {
+                if (data == null) {
+                    return;
+                }
+                Bundle bundle = data.getExtras();
+
+                CityInfoBean cityInfoBean = (CityInfoBean) bundle.getParcelable("cityinfo");
+
+                if (null == cityInfoBean)
+                    return;
+
+                //城市名称
+                String cityName = cityInfoBean.getName();
+                //纬度
+                String latitude = cityInfoBean.getLongitude();
+                //经度
+                String longitude = cityInfoBean.getLatitude();
+
+
+                tvResult.setText("城市： " + cityName + "\n" +
+                        "经度： " + latitude + "\n" +
+                        "纬度： " + longitude);
+            }
+        }
     }
 }
